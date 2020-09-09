a0c1ca4bac4c1ce0e29c4664a0bc0c2d69514dd2
Matthew Quint <mattquint111@gmail.com>
2020-09-09 12:53:16 -0500

    Modified Assignment 1 for each operand as seperate funciton

--- Assignment1-Calculator.py
+++ Assignment1-Calculator.py
@@ -1,13 +1,42 @@
-def calculator(first_number, operand, second_number):
-    soln = None
+# def calculator(first_number, operand, second_number):
+#     soln = None
 
-    if operand == '+':
-        soln = first_number + second_number
-    elif operand == '-':
-        soln = first_number - second_number
-    elif operand == '*':
-        soln = first_number * second_number
-    elif operand == '/':
-        soln = first_number / second_number
+#     if operand == '+':
+#         soln = first_number + second_number
+#     elif operand == '-':
+#         soln = first_number - second_number
+#     elif operand == '*':
+#         soln = first_number * second_number
+#     elif operand == '/':
+#         soln = first_number / second_number
     
-    return soln
+#     return soln
+
+
+input1 = int(input("Enter first number: "))
+input2 = input("Enter an operand (+,-,*,/): ")
+input3 = int(input("Enter second number: "))
+
+def add():
+    print(input1 + input3)
+
+def subtract():
+    print(input1 - input3)
+
+def multiply():
+    print(input1 * input3)
+
+def divide():
+    print(input1 / input3)
+
+def calculator():
+    if input2 == '+':
+        return add()
+    elif input2 == '-':
+        return subtract()
+    elif input2 == '*':
+        return multiply()
+    elif input2 == '/':
+        return divide()
+calculator()
+
