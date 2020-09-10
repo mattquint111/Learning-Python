5592f41846174c09aae0ebd86bbae947279048ba
Matthew Quint <mattquint111@gmail.com>
2020-09-10 11:18:20 -0500

    finished day3-assignment1-factorial

--- day3-assignment1.py
+++ day3-assignment1.py
@@ -1,1 +1,12 @@
-# Factorial
+# Assignment 1 - Factorial
+number = int(input("Enter a number: "))
+
+def factorial():
+    soln = 1
+
+    for i in range(1, number+1):
+        soln *= i
+
+    return soln
+
+print(factorial())
