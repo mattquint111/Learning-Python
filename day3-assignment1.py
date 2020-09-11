0e5b3b263e1ed02635eced48efb708410b21a940
Matthew Quint <mattquint111@gmail.com>
2020-09-11 08:59:01 -0500

    initial commit for week1 day4 lecture notes

--- day3-assignment1.py
+++ day3-assignment1.py
@@ -1,12 +1,27 @@
 # Assignment 1 - Factorial
-number = int(input("Enter a number: "))
+#number = int(input("Enter a non-negative number: "))
 
-def factorial():
+# def factorial():
+#     soln = 1
+
+#     for i in range(1, number+1):
+#         soln *= i
+        
+#     return soln
+
+#print(factorial())
+
+# Recursive factorial function 
+def factorial2(n):
+    # default solution for n == 0 (n! == 1)
     soln = 1
-
-    for i in range(1, number+1):
-        soln *= i
-
-    return soln
-
-print(factorial())
+    
+    if n == 0:
+        return soln
+    else:
+        # call factorial2() until n-1 == 0
+        soln = n * factorial2(n - 1)
+    
+    return soln  
+# 3! == 6
+print(factorial2(5))
--- day3-assignment3.py
+++ day3-assignment3.py
@@ -2,20 +2,17 @@
 
 number = int(input("Enter a number: "))
 
-
-
 def is_prime():
-    # declare an empty solution variable to return
+    # If a number is not proved to not be prime then by default number == prime
     soln = f"{number} IS PRIME"
     
-
+    # test to see if number is not prime
     if number <= 1:
         soln = f"{number} IS NOT PRIME"
     else:
         for i in range(2, number):
             if number % i == 0:
-                soln = f"{number} IS NOT PRIME"
-                
+                soln = f"{number} IS NOT PRIME"         
     return soln
 
 print(is_prime())
