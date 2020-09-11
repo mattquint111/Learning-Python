072d1634e9e538c924ae9c83ceb5315076d56082
Matthew Quint <mattquint111@gmail.com>
2020-09-11 13:26:10 -0500

    day4 assignment 4 finished

---
+++ day4-assignment4.py
@@ -0,0 +1,16 @@
+# Determine missing number in array
+
+test_list = [0, 1, 2, 3, 4, 5, 6, 8, 9]
+
+# Solution 1
+def find_missing(list):
+
+    for i in range(len(list) - 1):
+        if i not in list:
+            return i
+
+print(find_missing(test_list))
+
+
+# Solution 2
+print( sum(range(test_list[-1] + 1)) - sum(test_list))
