46faf68355cf30c484776d75da005d0d500d59ca
Matthew Quint <mattquint111@gmail.com>
2020-09-11 14:00:50 -0500

    updated assignment6 with function for pyramid of any size and character

--- day4-assignment6.py
+++ day4-assignment6.py
@@ -1,15 +1,32 @@
 # HARD Assignment : print out pyramid
 
 # initial conditions
-pyramin_size = 9
+pyramid_size = 9
 level = 1
 char_count = 1
 char = '*'
 space_count = 8
 space = ' '
 
-while level <= pyramin_size:
-    print( (space * space_count) + (char * char_count))
+while level <= pyramid_size:
+    print( (space * space_count) + (char * char_count) )
     level += 1
     char_count += 2
     space_count -= 1
+
+# print pyramid of any size with any character
+def print_pyramind(char, size):
+    pyramid_size = size
+    level = 1
+    char_count = 1
+    char = char
+    space_count = size - 1
+    space = ' '
+    
+    while level <= pyramid_size:
+        print((space * space_count) + (char * char_count))
+        level += 1
+        char_count += 2
+        space_count -= 1
+
+print_pyramind('#', 4)
